a105c1d0a56c2f0ce65fdb35e73a32454da94caf
Toan Nguyen <ntuantoan@gmail.com>
2018-03-17 08:11:17 +0700

    Feature: edit, delete a battle

--- app/controllers/battles_controller.rb
+++ app/controllers/battles_controller.rb
@@ -1,9 +1,9 @@
 class BattlesController < ApplicationController
   before_action :authenticate_user!, except: [:show]
-  before_action :battle_params, only: [:create, :update]
+  before_action :load_battle, except: [:new, :create]
+  before_action :owner, only: [:edit, :update, :destroy]
 
   def show
-    @battle = Battle.friendly.find params[:id]
   end
 
   def new
@@ -15,7 +15,7 @@ class BattlesController < ApplicationController
     if @battle.valid?
       @battle.save
       flash[:success] = "Battle is successfully created!"
-      redirect_to root_url
+      redirect_to @battle
     else
       render "new"
     end
@@ -25,11 +25,39 @@ class BattlesController < ApplicationController
   end
 
   def update
+    if @battle.update battle_params
+      flash[:success] = "Battle is successfully updated!"
+      redirect_back fallback_location: @battle
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    if @battle.destroy
+      flash[:success] = "Battle is successfully deleted!"
+      redirect_to root_url
+    else
+      flash[:danger] = "There was an error when trying to delete the battle!"
+      redirect_back fallback_location: root_url
+    end
   end
 
   private
 
   def battle_params
     params.require(:battle).permit :title, :link, :description
+  end
+
+  def load_battle
+    @battle = Battle.friendly.find params[:id]
+  end
+
+  # Filter: only owner can perform some certain actions
+  def owner
+    unless current_user == @battle.user
+      flash[:danger] = "You're not authorized to perform this action!"
+      redirect_back fallback_location: root_url
+    end
   end
 end
